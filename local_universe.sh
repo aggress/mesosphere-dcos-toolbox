9c2d94f69c8d286438443933e035ccb4ab1f2e5d
Richard Shaw <richard@aggress.net>
2017-09-14 16:59:46 +0100

    Finally got remove working without requiring a remote script. Quite ugly.

--- local_universe.sh
+++ local_universe.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # Builds a DC/OS local universe and pushes to a remote registry. Adds the
-# new universe to Marathon and makes available in the DC/OS Catalog
+# new universe to Marathon and makes available in the DC/OS Catalog.
+# Provides a cluster wide cleanup of the registry and all images for the
+# new universe and one defined package.
 #
 # Prerequisites:
 # 1. A local Docker daemon
@@ -18,7 +20,8 @@
 registry="192.168.33.13"
 registry_port="10000"
 c_path="/Users/richard/code"
-u_path="/Users/richard/code/universe"
+u_path="$c_path/universe"
+ssh_config="$c_path/mesosphere-dcos-vagrant-ansible/ssh-config"
 package=$2
 
 function new {
@@ -31,10 +34,12 @@ function new {
 function build {
   cd $u_path || exit
   scripts/build.sh
-  docker_image="$registry:$registry_port/universe-server" \
-    docker_tag="universe-server" docker/server/build.bash
-  docker_image="$registry:$registry_port/universe-server" \
-    docker_tag="universe-server" docker/server/build.bash publish
+  ssh -F $ssh_config public-agent -C "sudo docker run -d -p 10000:5000 \
+    --restart=always --name registry registry:2"
+  DOCKER_IMAGE="$registry:$registry_port/universe-server" \
+    DOCKER_TAG="universe-server" docker/server/build.bash
+  DOCKER_IMAGE="$registry:$registry_port/universe-server" \
+    DOCKER_TAG="universe-server" docker/server/build.bash publish
   dcos marathon app add $u_path/docker/server/target/marathon.json
   dcos package repo add \
     --index=0 universe-server http://universe.marathon.mesos:8085/repo
@@ -47,6 +52,16 @@ function remove {
   docker image rm -f $registry:$registry_port/universe-server:universe-server
   docker rmi -f "$(docker images -aq)"
   rm -f $u_path/target/*
+  # Yuck, needs proper error handling
+  ssh -F $ssh_config public-agent << 'EOF'
+    sudo docker ps -f name=registry -aq | xargs docker kill
+    sudo docker images | grep registry | awk {'print $3'} | xargs docker rmi -f
+    sudo docker images | grep "$package" | awk {'print $3'} | xargs docker rmi -f
+    sudo docker rm /registry
+EOF
+  ssh -F $ssh_config private-agent << 'EOF'
+    sudo docker images | grep universe-server | awk {'print $3'} | xargs docker rmi -f
+EOF
 }
 
 case "$1" in
